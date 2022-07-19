374d215ee856c0258e932851af89d5768f164efa
Rayhan F <70044074+Ray123fa@users.noreply.github.com>
2022-07-19 15:52:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -115,6 +115,13 @@
               <li><hr class="dropdown-divider"></li>
               <li>
                 <div class="dropdown-item mysocial">
+                  <span class="fab fa-twitter"></span>
+                  <a id="mysocial" href="https://www.twitter.com/xyz162ridh" style="font-weight: bold;">    Twitter</a>
+                </div>
+              </li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <div class="dropdown-item mysocial">
                   <span class="fab fa-line"></span>
                   <a id="mysocial" href="https://line.me/ti/p/~rayhanf1602" style="font-weight: bold;">    LINE</a>
                 </div>
